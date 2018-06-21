95eea997c598de2b60a7c38287814e1c68b34b9f
Alice Lapeyre <Alice@MBP-de-Alice.wifi-campus.warwick.ac.uk>
2018-06-21 15:33:30 +0100

    second commit

---
+++ ACS.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library("rgdal")
+library(cartography)
+
+setwd("/Users/Alice/Documents/ENSAE/2A/Warwick/Housing data/ACS")
+data_unit <- read.csv("ACS_16_5YR_B25003_with_ann.csv", header=T, sep=";")
+
+colnames(data_unit)
+data_unit <- data_unit %>%
+  mutate(
+    pct_owner_unit = HD01_VD02/HD01_VD01*100, 
+    pct_renter_unit= HD01_VD03/HD01_VD01*100, 
+    total_unit=pct_renter_unit + pct_owner_unit,
+    state = str_sub(GEO.id2,1,-4)
+    )
+
+data_pop <- read.csv("ACS_16_5YR_B25026_with_ann.csv", header=T, sep=";")
+colnames(data_pop)
+data_pop <- data_pop %>%
+  select(c(1:7),c(20:21)) %>%
+  mutate(
+    pct_owner_pop = HD01_VD02/HD01_VD01*100, 
+    pct_renter_pop= HD01_VD09/HD01_VD01*100, 
+    total_pop =pct_renter_pop + pct_owner_pop,
+    state = str_sub(GEO.id2,1,-4),
+  )
+
+as.character(data_unit$GEO.id2)
+data_ACS_county <- left_join(data_pop, data_unit, by=c("GEO.id2","state","GEO.id","GEO.display.label"))
+data_ACS_county$GEO.id2 <- as.character(data_ACS_county$GEO.id2)
+
+# removing Alaska and Puerto Rico 
+# data_ACS_county <- data_ACS_county %>%
+#  filter(!(str_sub(GEO.display.label,-6,-1)=="Alaska"))
---
+++ housing_COUNTY.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(openxlsx)
+library("rgdal")
+library(cartography)
+
+setwd("/Users/Alice/Documents/ENSAE/2A/Warwick/Housing data/PSH")
+data <- read.csv("COUNTY_2012.csv", header = T, sep = ";")
+
+colnames(data)
+
+data <- data %>%
+  filter(program_label == "Public Housing" & !(is.na(total_units))) %>%
+# & !(states=="PR Puerto Rico") & !(states == "AK Alaska")) 
+  select(states, entities, total_units, pct_occupied, people_per_unit, people_total, state) %>%
+  mutate(
+    GEO.id2 = str_sub(entities,-5,-1),
+    GEO.id2 = ifelse(str_sub(GEO.id2,1,1)==0, str_sub(GEO.id2,2,5),str_sub(GEO.id2,1,5))
+    ) %>%
+  filter()
+
+# join of the two data_frames
+data_combined <- full_join(data_ACS_county, data, by =("GEO.id2"))
+
+
+data_combined %>%
+  filter(is.na(HD01_VD01.x)) # NA sur GU Guam et VI Virgin Islands
+
+data_combined %>%
+  mutate(population_totale = sum(HD01_VD01.x, na.rm = T)) 
+# population totale = pop US hors PR et AK
+
+sub_sample <- data_combined %>%
+  filter(is.na(pct_occupied)) %>%
+  mutate(pop_no_info = sum(HD01_VD01.x))
+# pop manquante dans PSH = 13,7% de la pop totale
+
+mtq_county <- readOGR(dsn ="/Users/Alice/Downloads/tl_2012_us_county",layer = "tl_2012_us_county")
+
+
